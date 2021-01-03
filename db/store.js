a4c23321eb1621da5bb0ba67765b9ed925180d6f
Joey Sisk <jjmsisk@gmail.com>
2021-01-03 16:57:03 -0600

    fixed name of removeNote in store.js, working on connecting api, db.json, and browser inputs

--- db/store.js
+++ db/store.js
@@ -44,7 +44,7 @@ class Store {
     .then(() => newNote);
   }
 
-  removedNote(id) {
+  removeNote(id) {
     return this.getNotes()
       .then((notes) => notes.filter((note) => note.id !== id))
       .then((filteredNotes) => this.write(filteredNotes));
